bfcda0adca7d482a99729028c5fc0a20a469adc7
Abhay-Rana-18 <ranaabhay795@gmail.com>
2024-07-29 00:34:41 +0530

    error fixed

--- src/app/api/users/details/route.ts
+++ src/app/api/users/details/route.ts
@@ -1,6 +1,5 @@
 import { NextApiResponse } from "next";
 import { NextApiRequestWithUser } from "../../types/nextExtendedReq";
-import protect from "../../middleware/authMiddleware";
 import jwt from 'jsonwebtoken';
 import { User } from '@/app/db/user';
 import { ensureDbConnect } from "@/app/db/dbConnect";
@@ -9,29 +8,6 @@ import { cookies } from "next/headers";
 interface DecodedToken {
   id: string;
 }
-
-// Handler function to get user
-export const getUser = async (req: NextApiRequestWithUser, res: NextApiResponse) => {
-  console.log("Getting user details --->");
-  const secretKey = "secret123";
-  let email;
-  let token = cookies().get("token")?.value;
-  console.log("detail -> token: " + token);
-  if (token) {
-    try {
-      const decoded = jwt.verify(token, secretKey);
-      email = decoded?.email;
-      console.log("Emai: " + email);
-    } catch (error) {
-      console.error("Invalid or expired token", error);
-    }
-  } else {
-    console.log("No token found");
-  }
-  return Response.json({ success: true, message: "User details fetched successfully!", email: email })
-
-
-};
 
 // API route using GET method
 export async function GET(req: NextApiRequestWithUser, res: NextApiResponse) {
@@ -58,6 +34,3 @@ export async function GET(req: NextApiRequestWithUser, res: NextApiResponse) {
   }
 }
 
-// console.log("details");
-// const res2 = await protectMiddleware(req, res, getUser);
-// return res2;
